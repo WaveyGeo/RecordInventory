783a355f27dc33323541234fc298efc8f6957d50
WaveyGeo <87591@farnborough.ac.uk>
2019-10-11 12:09:15 +0100

    Added ps.set() functions to the User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -7,7 +7,10 @@ public class User {
 
         try {
             PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users(UserID, UserName, UserPassword) VALUES (?,?,?)");
-
+            ps.setInt(1, UserID);
+            ps.setString(2, UserName);
+            ps.setString(3, UserPassword);
+            ps.executeUpdate();
         } catch (Exception exception) {
             System.out.println("****** DATABASE ERROR: " + exception.getMessage() + " ******");
         }
