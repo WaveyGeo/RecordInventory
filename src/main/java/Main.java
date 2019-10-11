a8f63e7f14507acebc5e14e9e5bb1cb44b7cacd3
WaveyGeo <87591@farnborough.ac.uk>
2019-10-11 12:01:57 +0100

    Added the list all and close database functions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,12 +17,38 @@ public class Main {
             System.out.println("****** DATABASE CONNECTION ERROR: " + exception.getMessage() + " ******");
         }
     }
+    private static void closeDatabase(){
+        try {
+            db.close();
+            System.out.println("******Disconnected from database******");
+        } catch (Exception exception) {
+            System.out.println("******Database disconnection error: " + exception.getMessage() + "******");
+        }
+    }
+    private static void listAll() {
+        // code to get data from, write to the database etc goes here!
+        try {
+            PreparedStatement ps = db.prepareStatement("select UserID, UserName, UserPassword from Users");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()) {
+                int userID = results.getInt(1);
+                String userName = results.getString(2);
+                String userPassword = results.getString(3);
+                System.out.println("The user is " + userName + ", Password: " + userPassword + " (user ID: " + userID + ")");
+            }
+        } catch (Exception exception) {
+            System.out.println("Error In Database." + exception.getMessage());
+        }
+
+    }
 
     public static void main(String[] args) {
 
         openDatabase("RecordDatabase.db");
-
         User.insertUser(1,"TestyBrair","PAs3");
+        listAll();
+        closeDatabase();
 
 
     }
